e284e24dba59570483236f0be9451ac15e03c262
Emmanuel <kwamevaughan@gmail.com>
2024-11-03 15:33:25 +0000

    replaced with pg8000

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-# config.py
-import psycopg2
+import pg8000
 
 def get_db_connection():
-    connection = psycopg2.connect(
+    connection = pg8000.connect(
         host="aws-0-ap-south-1.pooler.supabase.com",
         database="postgres",
         user="postgres.wvbrwhnvvcpmlbuugumm",
@@ -10,5 +9,3 @@ def get_db_connection():
         port=5432
     )
     return connection
-
-#test
