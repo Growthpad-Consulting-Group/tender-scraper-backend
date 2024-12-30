16f47bf75afd3fa5331fc4a9655ca6a544e97867
Emmanuel <kwamevaughan@gmail.com>
2024-12-30 13:35:32 +0000

    fix socketio issue

--- app.py
+++ app.py
@@ -1,39 +1,36 @@
-# Import necessary libraries
 from flask import request, jsonify
 from dotenv import load_dotenv
-from flask_socketio import SocketIO
 from flask_jwt_extended import JWTManager
+from flask_socketio import SocketIO
 import logging
 import atexit
 import os
-from datetime import timedelta  # Import timedelta for setting expiration times
+from datetime import timedelta
 
-# Import custom modules and services
-from app import create_app, socketio  # Import socketio here if needed
+from app import create_app, socketio
 from app.services.scheduler import start_scheduler, shutdown_scheduler
 
-# Load environment variables from .env file
 load_dotenv()
 
 # Create Flask app
 app = create_app()  # This initializes the app
 
-# Configure logging for the application
+# Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# JWT setup: Retrieve secret key from environment variables
+# JWT setup
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=60)  # Access tokens expire in 60 minutes
-app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=14)     # Refresh tokens expire in 14 days
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=60)
+app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=14)
 
 # Initialize JWT with the app instance
 jwt = JWTManager(app)
 
-# Start background scheduler for periodic tasks and register graceful shutdown
+# Start scheduler
 start_scheduler()
 atexit.register(shutdown_scheduler)
 
-# Import and register application blueprints to handle different routes
+# Register blueprints
 from app.routes.keywords.keyword_routes import keyword_bp
 from app.routes.terms.search_terms import search_terms_bp
 from app.routes.upload.upload_routes import upload_bp
@@ -52,6 +49,6 @@ app.register_blueprint(countries_bp)
 app.register_blueprint(closing_keywords_bp)
 app.register_blueprint(scraping_log_bp)
 
-# Run the Flask application with SocketIO support
+# Only run this block if executed directly
 if __name__ == '__main__':
-    socketio.run(app, debug=True)  # Start the server with debug enabled
+    socketio.run(app, debug=True)
